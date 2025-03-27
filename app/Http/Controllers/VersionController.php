a16a270259ae94ee122b58211b273742963fa158
runth3 <runth3@yahoo.com>
2025-03-27 18:50:25 +0800

    Tes webhook pakai secret baru

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -10,7 +10,7 @@ class VersionController extends Controller
         return response()->json([
             'min_version' => '1.1.0',
             'update_url' => 'https://play.google.com/store/apps/details?id=com.example',
-            'maintenance' => false,
+            'maintenance' => true,
         ]);
     } 
 }
